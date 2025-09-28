d043fb47fa5f0468ff7a4c2d83f5f20d2b475c7a
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-09-27 19:35:05 -0700

    Working on print()

--- 210-lab-14.cpp
+++ 210-lab-14.cpp
@@ -1,6 +1,49 @@
 // COMSC-210 | Lab 14 | Dainiz Almazan
 // IDE used: CLion
+#include <iostream>
+#include <iomanip>
+using namespace std;
 
-/* In main(), create several Color objects, populate them with data, and output their values to the console in
- * neatly-formatted output using the object's print() method.
- */
+const int WIDTH = 10;
+
+class Color {
+	int red;
+	int green;
+	int blue;
+
+public:
+	int getRed() const { return red; }
+	int getGreen() const { return green; }
+	int getBlue() const { return blue; }
+	void setRed(int red) { this->red = red; }
+	void setGreen(int green) { this->green = green; }
+	void setBlue(int blue) { this->blue = blue; }
+	void print() {
+		cout << setw(WIDTH) << "COLOR " << "VALUE" << endl;
+		cout << setw(WIDTH) << "Red " << red << endl;
+		cout << setw(WIDTH) << "Green " << green << endl;
+		cout << setw(WIDTH) << "Blue " << blue << endl << endl;
+	}
+};
+
+int main() {
+	Color orange;
+	orange.setRed(255);
+	orange.setGreen(128);
+	orange.setBlue(0);
+	orange.print();
+
+	Color yellow;
+	yellow.setRed(255);
+	yellow.setGreen(255);
+	yellow.setBlue(0);
+	yellow.print();
+
+	Color purple;
+	purple.setRed(127);
+	purple.setGreen(0);
+	purple.setBlue(255);
+	purple.print();
+
+	return 0;
+}
